64fa4531e6c0f7e05c7cb255ad41718490dd513f
HannaBatarchuk <h.batarchuk@gmail.com>
2021-11-17 23:59:33 +0200

    HW 7 (Task-1__gallery)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryItemsList = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+.map(({preview, original, description}) => `<a class="gallery__link" href=${original}>
+    <img
+        class="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt=${description}
+    />
+    </a>`)
+.join("");
+
+galleryItemsList.innerHTML = galleryMarkup;
+// console.log(galleryMarkup);
+
+galleryItemsList.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+    event.preventDefault();
+
+    const filterSource = event.target.dataset.source;
+    if (!filterSource) return;
+
+    console.log(filterSource)
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+        <img class="modal-img" src="${filterSource}">
+    </div>`, {
+    onShow: (instance) => {
+        instance.element().querySelector('img').onclick = instance.close;
+        }
+    })
+    instance.show()
+}
+
