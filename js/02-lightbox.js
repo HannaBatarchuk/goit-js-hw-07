f71e76df0fbd0055e3f931bc305771ef4a8be2eb
HannaBatarchuk <h.batarchuk@gmail.com>
2021-11-18 17:23:01 +0200

    HW 7 (Task 2__lightbox)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryItemsList = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+.map(({preview, original, description}) => `<a class="gallery__item" href="${original}">
+    <img 
+        class="gallery__image" 
+        src="${preview}" 
+        alt="${description}" 
+    />
+</a>`)
+.join("");
+
+galleryItemsList.innerHTML = galleryMarkup;
+// console.log(galleryMarkup);
+
+galleryItemsList.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+    event.preventDefault();
+
+    const filterSource = event.target.dataset.source;
+    if (!filterSource) return;
+
+    console.log(filterSource)
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData : "alt", captionDelay : 250});
+lightbox.on("show.simplelightbox");
